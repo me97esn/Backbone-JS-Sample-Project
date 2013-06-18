a1bc57cc273355f50f7122ce993a13943762a91e
Emil Stenberg <esandin@gmail.com>
2013-06-18 11:49:37 +0200

    changed the data to more real examples

--- grailsproject/BackEnd/grails-app/conf/BootStrap.groovy
+++ grailsproject/BackEnd/grails-app/conf/BootStrap.groovy
@@ -34,64 +34,55 @@ class BootStrap {
             return returnArray
         }
 
-        new Year(amount: 1000, yearfield: 2013, activityCollection: [
-                new Activity(name: 'An Activity', amount: 10000, expenseCollection: [
-                        new Expense(name: "Lön", amount: 100, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
-                        ]),
-                        new Expense(name: "Resor", amount: 9900, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
-                        ])
-
-                ]),
-                new Activity(name: 'Another Activity', amount:4400, expenseCollection: [
-                        new Expense(name: "Lön", amount: 9800, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
-                        ]),
-                        new Expense(name: "Resor", amount: 1000000, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
-                        ])
-
-                ])
-        ]
-        ).save()
         new Year(amount: 1000, yearfield: 2012, activityCollection: [
-                new Activity(name: 'An Activity', amount: 10000, expenseCollection: [
-                        new Expense(name: "Lön", amount: 100, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
+                new Activity(name: 'Aktiviteter riktade till målgruppen', amount: 10000, expenseCollection: [
+                        new Expense(name: "Löner personal", amount: 100, budgetEntryCollection: [
+                                new BudgetEntry(name: "Jan Zachariassen", amount: 100000, description: "Jan Zachariassens månadslön"),
+                                new BudgetEntry(name: "Greger Sernemar", amount: 90000, description: "Greger Sernemars månadslön"),
+                                new BudgetEntry(name: "Per Bodin", amount: 78000, description: "Per Bodins månadslön"),
                         ]),
-                        new Expense(name: "Resor", amount: 9900, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
+                        new Expense(name: "Lokaler", amount: 9900, budgetEntryCollection: [
+                                new BudgetEntry(name: "Lokalhyra Siemens Upplands Väsby", amount: 1000000, description: "Lokalhyra Siemens Upplands Väsby") ,
                         ])
 
                 ]),
-                new Activity(name: 'Another Activity', amount:4400, expenseCollection: [
-                        new Expense(name: "Lön", amount: 9800, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
+                new Activity(name: 'Information och marknadsföring', amount:4400, expenseCollection: [
+                        new Expense(name: "Reklam", amount: 9800, budgetEntryCollection: [
+                                new BudgetEntry(name: "Reklam tv4", amount: 1000000, description: "Reklam tv4"),
+                                new BudgetEntry(name: "Reklamkampanj tunnelbanan", amount: 1500000, description: "Reklamkampanj tunnelbanan") ,
                         ]),
-                        new Expense(name: "Resor", amount: 1000000, budgetEntryCollection: [
-                                new BudgetEntry(name: "Emil", amount: 555, description: "Emils lilla lön"),
-                                new BudgetEntry(name: "Jan", amount: 1000000, description: "Jannes massiva lön") ,
-//                                new BudgetEntry(name: "Greger", amount: 555)
+                ])
+        ]
+        ).save()
+        new Year(amount: 1000, yearfield: 2013, activityCollection: [
+                new Activity(name: 'Aktiviteter till målgruppen', amount: 10000, expenseCollection: [
+                        new Expense(name: "Löner personal", amount: 100, budgetEntryCollection: [
+                                new BudgetEntry(name: "Emil Stenberg", amount: 800, description: "Emils månadslön"),
+                                new BudgetEntry(name: "Jan Zachariassen", amount: 100000, description: "Jan Zachariassens månadslön"),
+                                new BudgetEntry(name: "Greger Sernemar", amount: 90000, description: " Greger Sernemars månadslön"),
+                                new BudgetEntry(name: "Per Bodin", amount: 78000, description: "Per Bodins månadslön"),
+                        ]),
+                        new Expense(name: "Lokaler", amount: 9900, budgetEntryCollection: [
+                                new BudgetEntry(name: "Lokalhyra hantverkargatan", amount: 555, description: "Lokalhyra hantverkargatan"),
+                                new BudgetEntry(name: "Lokalhyra Siemens Upplands Väsby", amount: 1000000, description: "Lokalhyra Siemens Upplands Väsby") ,
+                        ])
+
+                ]),
+                new Activity(name: 'Information och marknadsföring', amount:4400, expenseCollection: [
+                        new Expense(name: "Reklam", amount: 9800, budgetEntryCollection: [
+                                new BudgetEntry(name: "Reklam tv4", amount: 1000000, description: "Reklam tv4"),
+                                new BudgetEntry(name: "Reklamkampanj tunnelbanan", amount: 1500000, description: "Reklamkampanj tunnelbanan") ,
+                                new BudgetEntry(name: "Reklamkampanj SVD", amount: 200000, description: "Reklamkampanj SVD") ,
+                                new BudgetEntry(name: "Reklamkampanj Reklam radio", amount: 300000, description: "Radioreklam") ,
+                        ]),
+                        new Expense(name: "PR", amount: 1000000, budgetEntryCollection: [
+                                new BudgetEntry(name: "PR-kampanj hos 'PR och Reklam AB'", amount: 125000, description: "PR-kampanj hos 'PR och Reklam AB"),
                         ])
 
                 ])
         ]
         ).save()
+                .save()
 
     }
     def destroy = {
